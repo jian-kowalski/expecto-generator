497dddd3274a7841cc7736e9a43c1bd394df6191
jian <jian_kowalski@hotmail.com>
2022-05-26 22:18:28 -0300

    configuração inicial do projeto

---
+++ src/main/java/com/jiankowalski/expectro/ExpectroApplication.java
@@ -0,0 +1,13 @@
+package com.jiankowalski.expectro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpectroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpectroApplication.class, args);
+	}
+
+}
